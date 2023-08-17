d3b03f0222300343d3523692f844faa8968ec4d6
Ettore Alessandro <ettorerollo@hotmail.com>
2023-08-17 01:29:07 -0300

    Teste para classe user service método findById objeto não encontrado

---
+++ src/test/java/com/study/api/services/impl/UserServiceImplTest.java
@@ -0,0 +1,117 @@
+package com.study.api.services.impl;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.study.api.domain.User;
+import com.study.api.domain.dto.UserDTO;
+import com.study.api.repositories.UserRepository;
+import com.study.api.services.exceptions.ObjectNotFoundException;
+
+@SpringBootTest
+public class UserServiceImplTest {
+
+    /**
+     * VARIÁVEL ID PARA INICIAR OS OBJETOS
+     */
+    private static final Integer ID = 1;
+
+    /**
+     * VARIÁVEL NAME PARA INICIAR OS OBJETOS
+     */
+    private static final String NAME = "Teste";
+
+    /**
+     * VARIÁVEL EMAIL PARA INICIAR OS OBJETOS
+     */
+    private static final String EMAIL = "teste@teste.com";
+
+    /**
+     * VARIÁVEL PASSWORD PARA INICIAR OS OBJETOS
+     */
+    private static final String PASSWORD = "123";
+
+    /**
+     * Mensagem de usuário não encontrado
+     */
+    private static final String USUÁRIO_NÃO_ENCONTRADO = "Usuário não encontrado.";
+
+    @InjectMocks
+    private UserServiceImpl service;
+
+    @Mock
+    private UserRepository repository;
+
+    private User user;
+    private UserDTO userDTO;
+    private Optional<User> optionalUser;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void testCreateUser() {
+
+    }
+
+    @Test
+    void testDeleteUser() {
+
+    }
+
+    @Test
+    void testFindAll() {
+
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnUserInstance() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+
+        User response = service.findById(ID);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnObjectNotFoundException() {
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(USUÁRIO_NÃO_ENCONTRADO));
+
+        try {
+            service.findById(ID);
+        } catch(Exception ex) {
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals(USUÁRIO_NÃO_ENCONTRADO, ex.getMessage());
+        }
+    }
+
+    @Test
+    void testUpdateUser() {
+
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+        optionalUser = Optional.of(new User(ID, NAME, EMAIL, PASSWORD));
+    }
+}
