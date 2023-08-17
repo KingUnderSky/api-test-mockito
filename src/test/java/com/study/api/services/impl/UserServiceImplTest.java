2e636c0a51185dda2ed3a4093db8c33b98e8794e
Ettore Alessandro <ettorerollo@hotmail.com>
2023-08-17 02:05:51 -0300

    teste classe user service método delete sucesso

--- src/test/java/com/study/api/services/impl/UserServiceImplTest.java
+++ src/test/java/com/study/api/services/impl/UserServiceImplTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -105,8 +108,11 @@ public class UserServiceImplTest {
     }
 
     @Test
-    void testDeleteUser() {
-
+    void deleteWithSuccess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyInt());
+        service.deleteUser(ID);
+        verify(repository, times(1)).deleteById(anyInt());
     }
 
     @Test
